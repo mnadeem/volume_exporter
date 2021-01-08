63717c4e10f6416b197183e45a723214cda4dfbe
Nadeem Mohammad <nadeembtech@gmail.com>
2021-01-09 01:15:53 +0530

    tests

--- exporter/volume_exporter_test.go
+++ exporter/volume_exporter_test.go
@@ -1,6 +1,8 @@
 package exporter_test
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 
 	"github.com/mnadeem/volume_exporter/exporter"
@@ -24,11 +26,45 @@ func TestDesc(t *testing.T) {
 	for i := 1; i <= 4; i++ {
 		m := <-desc
 		if m == nil {
+			t.Error("expected desc but got nil")
+		}
+	}
+	extraDesc := 0
+	for <-desc != nil {
+		extraDesc++
+	}
+	if extraDesc > 0 {
+		t.Errorf("expected closed channel, got %d extra desc", extraDesc)
+	}
+}
+
+func TestCollect(t *testing.T) {
+	path, err := ioutil.TempDir(os.TempDir(), "disk-")
+	defer os.RemoveAll(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	volOpts := exporter.VolumeOpts{}
+	volOpts.Options = append(volOpts.Options, exporter.VolumeOpt{Name: "temp", Path: path})
+
+	collector := exporter.NewVolumeCollector(&volOpts)
+
+	ch := make(chan prometheus.Metric)
+
+	go func() {
+		defer close(ch)
+		collector.Collect(ch)
+	}()
+
+	for i := 1; i <= 4; i++ {
+		m := <-ch
+		if m == nil {
 			t.Error("expected metric but got nil")
 		}
 	}
 	extraMetrics := 0
-	for <-desc != nil {
+	for <-ch != nil {
 		extraMetrics++
 	}
 	if extraMetrics > 0 {
